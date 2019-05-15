e3be5eeb024f4e131a5bb0ca9d44021ebf12e1a7
Yagnadat-Talakola <talakola.yagna@gmail.com>
2019-05-15 16:21:37 -0500

    worked on Ex until 1.33

--- Ch1/Ex-1.12.rkt
+++ Ch1/Ex-1.12.rkt
@@ -12,5 +12,50 @@
         ((= col row) 1)
         (else (+ (pascal (- row 1) (- col 1))
                  (pascal (- row 1) col)))))
-     
-             
+
+
+; sum procedure as an iterative process.
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (cube x) (* x x x))
+(define (inc x) (+ x 1))
+(define (identity x) x)
+
+; product procedure of a function computed for a range of values from a to b.
+(define (product f a next b)
+  (if (> a b)
+      1
+      (* (f a)
+         (product f (next a) next b))))
+
+; factorial in-terms of product procedure
+(define (factorial b)
+  (product identity 1 inc b))
+
+; factorial as an iterative process
+(define (fact-iter b)
+  (define (iter count result)
+    (if (> count b)
+        result
+        (iter (+ count 1) (* result count))))
+  (iter 1 1))
+
+; accumulate procedure as an abstraction for combining terms
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+; accumulate procedure as an iterative process
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter x result)
+    (if (> x b)
+        result
+        (iter (next x) (combiner result (term x)))))
+  (iter a null-value))
+
