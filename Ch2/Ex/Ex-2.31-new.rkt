5fe44818c2050b1a0f20dd253643d1faa7738b3e
Yagna Talakola <yagnatalakola@Yagnas-MacBook-Air.local>
2020-05-27 14:04:33 -0500

    tree-map procedure

---
+++ Ch2/Ex/Ex-2.31-new.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(define square (lambda (x) (* x x)))
+
+(define (square-tree tree) 
+  (tree-map square tree))
+
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
